5c243485f2d5e2f8731b2114c7c44675a4dc288c
Peter Prosol <peter.prosol@gmail.com>
2015-07-16 20:18:51 -0500

    1.BasicMapKit

---
+++ Map Kit Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Map Kit Test
+//
+//  Created by Piotr Prosol on 7/16/15.
+//  Copyright (c) 2015 Piotr Prosol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Map Kit Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Map Kit Test
+//
+//  Created by Piotr Prosol on 7/16/15.
+//  Copyright (c) 2015 Piotr Prosol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
